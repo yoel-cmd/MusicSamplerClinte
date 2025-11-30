340fb4068513118a8658acdb971c703f2ae981ef
yoel ider <yoeli5746@gmail.com>
2025-11-30 18:02:16 +0200

    creat component single div

--- src/components/layout/Div.tsx
+++ src/components/layout/Div.tsx
@@ -1,1 +1,77 @@
-//ghgjghjdgh
+// import { useState } from "react";
+
+import { useContext } from "react";
+import { Logic } from "../../context/logic";
+import ColorsRow from "../../util/ColorsRow.ts";
+
+// interface DivsProps {
+//   name: string;
+//   classname: string;
+// }
+// const Divs = ({ name }: DivsProps) => {
+//   //   const [press, setPress] = useState(false);
+//   const [isUrl, setIsUrl] = useState(null);
+
+//   const onClick = async () => {
+//     if (isUrl) {
+//       setIsUrl(null);
+//       return;
+//     }
+//     try {
+//       const respons = await fetch("http://localhost:3000/load-image");
+//       if (!respons.ok) {
+//         throw new Error("error load to server");
+//       }
+//       const data = await respons.json();
+//       console.log(data.url);
+//       setIsUrl(data.url);
+//     } catch (error) {
+//       console.error("errorr:", error);
+//     }
+//   };
+//   return (
+//     <>
+//       <div
+//         className={isUrl ? "grid-item" : "dark"}
+//         onClick={onClick}
+//         style={
+//           isUrl
+//             ? { backgroundImage: `url(http://localhost:3000/${isUrl})` }
+//             : undefined
+//         }
+//       >
+//         {!isUrl && <span>enter to load image</span>}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Divs;
+interface Props {
+  activ: boolean;
+  row: number;
+  col: number;
+}
+
+export const Div = ({ activ, row, col }: Props) => {
+  const context = useContext(Logic);
+  if (!context) {
+    throw new Error("error");
+  }
+  const { onToggle, url, musical,currentColumn,urlSupa } = context;
+  const color = ColorsRow(row);
+
+  const onClick = () => {
+    onToggle(col, row);
+    new Audio(urlSupa[row]).play();
+    
+  };
+
+  return (
+    <div
+      className={`grid-music-div ${col===currentColumn ? "currentColumn" : ""}`}
+      style={{ background: activ ? color : undefined }}
+      onClick={onClick}
+    ></div>
+  );
+};
