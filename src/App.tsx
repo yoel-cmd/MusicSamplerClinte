3a7633b5d4399b7a431265043ae3aa823aaa750a
yoel ider <yoeli5746@gmail.com>
2025-11-30 18:09:03 +0200

    created App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import "./App.css";
+import { Home } from "./components/layout/Home";
 
-import './App.css'
+import { LogicProvide } from "./context/logic";
 
 function App() {
-  
-
   return (
     <>
-      
+      <LogicProvide>
+        {/* <Home countOfDivs={70}/> */}
+        <Home/>
+      </LogicProvide>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
