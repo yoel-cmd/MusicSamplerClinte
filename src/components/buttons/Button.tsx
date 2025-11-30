0b9e17d4c372abd21f0ead9b3851980395a04e7f
yoel ider <yoeli5746@gmail.com>
2025-11-30 17:56:35 +0200

    create button logic

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,1 +1,9 @@
-//hgjghkghd
+import type IButton from "../../interface/ButtonInterface";
+
+export const Button = ({className, text, onClick}: IButton) => {
+  return (
+    <>
+      <button className={className} onClick={onClick}>{text}</button>
+    </>
+  );
+};
