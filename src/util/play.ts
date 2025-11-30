a36c48f008c389f23dbd424904b895843a91a33a
yoel ider <yoeli5746@gmail.com>
2025-11-30 18:07:31 +0200

    create function Play

--- src/util/play.ts
+++ src/util/play.ts
@@ -1,1 +1,32 @@
-//dkfjdghdhfgdfig
+
+interface PlayProps {
+    currentColumn: number;
+    setCurrentColumn: React.Dispatch<React.SetStateAction<number>>;
+    toggleRunning: () => void;
+    gridMatrix: boolean[][];
+    urlSupa: string[];
+    musical: string;
+    volumeRef: React.RefObject<number>;
+    speedRef: React.RefObject<number>;
+}
+
+const play = async ({ currentColumn, setCurrentColumn, toggleRunning, gridMatrix, urlSupa, musical, volumeRef, speedRef }: PlayProps) => {
+  if (currentColumn < 0) return;
+//   toggleRunning();
+console.log("currentColumn",currentColumn);
+  for (let row = 0; row < gridMatrix[currentColumn].length; row++) {
+    
+    if (gridMatrix[currentColumn][row]) {
+      const currentAudio = new Audio(urlSupa[row]);
+      currentAudio.volume = volumeRef.current;
+      currentAudio.play();
+    }
+}
+await new Promise((res) => setTimeout(res, speedRef.current));
+  if (currentColumn===gridMatrix.length-1) {
+        setCurrentColumn(0)
+  }
+  setCurrentColumn((prev)=>prev+1)
+  
+}
+export default play;
