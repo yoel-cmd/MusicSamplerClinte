d2b7ddb1dbc01ffb87b8029eda68b97c95bc44f0
yoel ider <yoeli5746@gmail.com>
2025-11-30 18:05:54 +0200

    created function to color row

--- src/util/ColorsRow.ts
+++ src/util/ColorsRow.ts
@@ -1,1 +1,18 @@
-//gjghjghjghj
+export default function ColorsRow(row: number) {
+  switch (row) {
+    case 0:
+      return "#FF8C94";
+    case 1:
+      return "#FFB6A0";
+    case 2:
+      return "#FEEFAD";
+    case 3:
+      return "#A8DAD0";
+    case 4:
+      return "#9BBFE8";
+    case 5:
+      return "#E0C2E8";
+    case 6:
+      return "#B19F9F";
+  }
+}
